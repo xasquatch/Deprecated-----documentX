af8f73fad7bb709dcb8ac87329865dbcabde89da
xasquatch <bigfoot7774@gmail.com>
2021-05-10 10:27:42 +0900

    - 이메일센더 설정 및 sql멤버매퍼 설정 - properties 설정으로 salt값 주입

--- src/main/java/net/xasquatch/document/config/CustomRootAppContext.java
+++ src/main/java/net/xasquatch/document/config/CustomRootAppContext.java
@@ -1,14 +1,22 @@
 package net.xasquatch.document.config;
 
+import net.xasquatch.document.mapper.MemberMapper;
+import net.xasquatch.document.model.Member;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperFactoryBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.web.context.annotation.SessionScope;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+
+import java.util.Properties;
 
 @Configuration
 @PropertySource("/WEB-INF/setting.properties")
@@ -24,15 +32,21 @@ public class CustomRootAppContext {
     @Value("${db.password}")
     private String dbPassword;
 
-//    /*--------TODO:Mail 접속관리--------------*/
-//    @Value("${email.host}")
-//    private String emailHost;
-//    @Value("${email.port}")
-//    private int emailPort;
-//    @Value("${email.username}")
-//    private String emailUserName;
-//    @Value("${email.password}")
-//    private String emailPwd;
+    /*--------TODO:Mail 접속관리--------------*/
+    @Value("${email.host}")
+    private String emailHost;
+    @Value("${email.port}")
+    private String emailPort;
+    @Value("${email.username}")
+    private String emailUserName;
+    @Value("${email.password}")
+    private String emailPwd;
+
+    @Bean("sessionMember")
+    @SessionScope
+    public Member loginMember() {
+        return new Member();
+    }
 
 
     @Bean
@@ -55,6 +69,14 @@ public class CustomRootAppContext {
 
     }
 
+    @Bean
+    public MapperFactoryBean<MemberMapper> getMemberMapper(SqlSessionFactory factory) {
+        MapperFactoryBean<MemberMapper> factoryBean = new MapperFactoryBean<MemberMapper>(MemberMapper.class);
+        factoryBean.setSqlSessionFactory(factory);
+        return factoryBean;
+
+    }
+
 
     //------------------------------------------------------
     @Bean
@@ -62,7 +84,6 @@ public class CustomRootAppContext {
         return new StandardServletMultipartResolver();
     }
 
-/*
     @Bean
     public ReloadableResourceBundleMessageSource msgSource() {
         ReloadableResourceBundleMessageSource msg = new ReloadableResourceBundleMessageSource();
@@ -85,13 +106,12 @@ public class CustomRootAppContext {
         mailSender.setJavaMailProperties(mailProperties);
         mailSender.setDefaultEncoding("UTF-8");
         mailSender.setHost(emailHost);
-        mailSender.setPort(emailPort);
+        mailSender.setPort(Integer.parseInt(emailPort));
         mailSender.setUsername(emailUserName);
         mailSender.setPassword(emailPwd);
 
         return mailSender;
     }
-*/
 
 
 }
--- src/main/java/net/xasquatch/document/repository/MemberDao.java
+++ src/main/java/net/xasquatch/document/repository/MemberDao.java
@@ -1,9 +1,21 @@
 package net.xasquatch.document.repository;
 
+import net.xasquatch.document.mapper.MemberMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@PropertySource("/WEB-INF/setting.properties")
 public class MemberDao {
+
+    @Autowired
+    private MemberMapper memberMapper;
+
+    @Value("${password.encryption.salt}")
+    private String salt;
+
 
 
 
