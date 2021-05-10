5f1b7b30619f59e7b9ad6bdc1a9cf6fafad50d4d
xasquatch <bigfoot7774@gmail.com>
2021-05-10 10:28:14 +0900

    userDatiails 인터페이스 모델객체에 구현

--- src/main/java/net/xasquatch/document/model/Member.java
+++ src/main/java/net/xasquatch/document/model/Member.java
@@ -1,4 +1,77 @@
 package net.xasquatch.document.model;
 
-public class Member {
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.util.Collection;
+import java.util.Date;
+
+@Configuration
+@Getter
+@Setter
+public class Member implements UserDetails {
+
+    private Long no;
+
+    @NotNull
+    @Pattern(regexp = "/^[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*\\.[a-zA-Z]{2,3}$/")
+    private String email;
+
+    @NotNull
+    @Pattern(regexp = "/^[a-z0-9]{8,20}$/")
+    @Size(min = 8,max = 20)
+    private String pwd;
+
+    @Size(min = 8,max = 20)
+    @Pattern(regexp = "/^[0-9a-z]{8,20}$/")
+    private String nickName;
+    private Date createdDate;
+
+    private int failCount;
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+    private boolean enable;
+    private Collection <? extends GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.pwd;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return this.credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enable;
+    }
 }
