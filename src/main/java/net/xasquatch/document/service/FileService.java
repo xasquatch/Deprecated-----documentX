fc64b664a09dcfd8e53d56bf3b2bd75982e15010
xasquatch <bigfoot7774@gmail.com>
2021-05-12 00:49:22 +0900

    파일IO 서비스 구현

--- src/main/java/net/xasquatch/document/service/FileService.java
+++ src/main/java/net/xasquatch/document/service/FileService.java
@@ -1,8 +1,86 @@
 package net.xasquatch.document.service;
 
 import net.xasquatch.document.service.command.FileServiceInterface;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
+@PropertySource("WEB-INF/setting.properties")
 public class FileService implements FileServiceInterface {
+
+    @Value("${files.save.path}")
+    private String filesSavePath;
+    @Value("${files.context.path}")
+    private String filesContextPath;
+
+    //생성된 경로 리턴
+    @Override
+    public String writeFile(byte[] byteArray, String path, String saveFileName) {
+        File filedir = new File(this.filesSavePath + path);
+        String filePath = this.filesSavePath + path + File.separator + saveFileName;
+        filedir.mkdirs();
+
+        try (BufferedOutputStream bytebuffer = new BufferedOutputStream(new FileOutputStream(filePath));) {
+
+            bytebuffer.write(byteArray);
+            filePath = this.filesContextPath + path + File.separator + saveFileName;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        filePath = filePath.replace(File.separator, "/");
+
+        return filePath;
+    }
+
+    @Override
+    public boolean isExistedFile(String path) {
+        return new File(this.filesSavePath + path).exists();
+    }
+
+    @Override
+    public boolean removeFiles(String path) {
+        boolean result = false;
+        if (isExistedFile(path))
+            result = new File(this.filesSavePath + path).delete();
+
+        return result;
+
+    }
+
+    //이미지 업로드하고 업로드 된 경로를 리스트에 담아 리턴
+    @Override
+    public List<String> uploadFile(MultipartHttpServletRequest request, String memberNo) {
+        List<String> resultFileList = new ArrayList<String>();
+        List<MultipartFile> multipartFiles = request.getFiles("filePackage");
+
+        for (MultipartFile multipartFile : multipartFiles) {
+            if (multipartFile != null) {
+
+                try {
+                    String targetName = multipartFile.getOriginalFilename();
+                    String contextPath = writeFile(multipartFile.getBytes(), File.separator + memberNo, targetName);
+                    resultFileList.add(contextPath);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+
+
+        return resultFileList;
+    }
 }
--- src/main/java/net/xasquatch/document/service/command/FileServiceInterface.java
+++ src/main/java/net/xasquatch/document/service/command/FileServiceInterface.java
@@ -1,6 +1,16 @@
 package net.xasquatch.document.service.command;
 
+
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import java.util.List;
+
 public interface FileServiceInterface {
 
-
+    //생성된 경로 리턴
+    String writeFile(byte[] byteArray, String path, String saveFileName);
+    boolean isExistedFile(String path);
+    boolean removeFiles(String path);
+    //이미지 업로드하고 업로드 된 경로를 리스트에 담아 리턴
+    List<String> uploadFile(MultipartHttpServletRequest request, String memberNo);
 }
