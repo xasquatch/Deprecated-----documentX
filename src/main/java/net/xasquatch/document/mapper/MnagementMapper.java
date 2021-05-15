d6c5a29658802b655c2f297575ab5cdf19c0b6da
xasquatch <bigfoot7774@gmail.com>
2021-05-15 12:56:38 +0900

    멤버 조회 빌더 생성중

--- src/main/java/net/xasquatch/document/mapper/MnagementMapper.java
+++ src/main/java/net/xasquatch/document/mapper/MnagementMapper.java
@@ -1,4 +1,10 @@
 package net.xasquatch.document.mapper;
 
+import net.xasquatch.document.model.Member;
+
+import java.util.List;
+
 public interface MnagementMapper {
+
+    List<Member> selectMemberList(String searchValue);
 }
--- src/main/java/net/xasquatch/document/mapper/builder/MemberBuilder.java
+++ src/main/java/net/xasquatch/document/mapper/builder/MemberBuilder.java
@@ -1,4 +1,36 @@
 package net.xasquatch.document.mapper.builder;
 
+import org.apache.ibatis.jdbc.SQL;
+
 public class MemberBuilder {
+
+    public static final String selectMemberList(String searchValue) {
+        return new SQL() {{
+            //SQL 조건 설정
+            boolean whereCase = true;
+            if (searchValue == null || searchValue.equals(""))
+                whereCase = false;
+
+            //QUERY
+            SET("@rownum:=0");
+            SELECT("(@rownum:= @rownum + 1) AS rownum, " +
+                    "m.no AS no, m.email AS email, m.pwd AS pwd, m.nick_name AS nick_name, " +
+                    "a.name AS authorization");
+            FROM("mbr m");
+            RIGHT_OUTER_JOIN("authorization a ON m.no = a.mbr_no");
+            if (whereCase) {
+                WHERE("m.email LIKE '%memberEmail%'");
+                OR();
+                WHERE("m.email LIKE '%memberName%'");
+                OR();
+                WHERE("a.name LIKE '%Auth%'");
+            }
+            ORDER_BY("rownum DESC");
+        }}.toString();
+    }
+
+    public static void main(String[] args) {
+        String query = MemberBuilder.selectMemberList("test");
+        System.out.println(query);
+    }
 }
